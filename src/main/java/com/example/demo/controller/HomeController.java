7203e86ba55511888d64d42e45393216734b1d8a
divyaraj22 <20it030@charusat.edu.in>
2023-03-02 16:05:59 +0530

    uploaded from divy

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World , My Name is Malav Champaneria and ID is 20it013";
+        return "Hello, World , My Name is Divyaraj dodiya and ID is 20it030";
     }
 
 }
