a884c3a340ae73c663f81fca53784a058ab2a39e
yashraj makwana <20it065@charusat.edu.in>
2023-03-13 22:52:27 +0530

    commit from yashraj

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World , My Name is Malav Champaneria and ID is 20it013";
+        return "Hello, World , My Name is Yashraj Makwana and ID is 20it065";
     }
 
 }
